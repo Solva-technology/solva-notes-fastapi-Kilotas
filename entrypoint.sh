22d132e35ef1dd40441d253080917c572916e82d
Kilotas <Alendos87@gmail.com>
2025-08-19 15:39:07 +0500

    поправка в ревью

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,12 @@
 set -e
 
 echo ">> Waiting for Postgres at ${POSTGRES_HOST}:${POSTGRES_PORT}..."
-# nc в Debian/Ubuntu пакете называется netcat-openbsd; в slim обычно есть
 until nc -z "${POSTGRES_HOST:-db}" "${POSTGRES_PORT:-5432}"; do
   sleep 0.5
 done
 echo ">> Postgres is up"
 
-# Применим миграции, если есть Alembic
+
 if [ -f "alembic.ini" ]; then
   echo ">> Running Alembic migrations"
   alembic upgrade head
