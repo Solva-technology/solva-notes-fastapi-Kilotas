b1a48a23e554061ac164df1bcf555cc00a2b5c34
Kilotas <Alendos87@gmail.com>
2025-09-05 18:38:19 +0500

    правки

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from logging.config import fileConfig
 
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]  # /app
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
